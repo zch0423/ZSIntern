f3633d0b320be30e6c8f4ae73abc94ef4ac70c68
zch0423 <zch0423@foxmail.com>
2021-01-26 09:13:15 +0800

    Jan 25 achieve nothing

--- test.py
+++
@@ -1,9 +0,0 @@
-# # 中文字体
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
-# zhfont = mpl.font_manager.FontProperties(
-#     fname="/System/Library/Fonts/PingFang.ttc")
-# fig, axs = plt.subplots()
-# axs.set_title(u"这是标题", fontproperties=zhfont)
-# # fig.show()
-# plt.show()
